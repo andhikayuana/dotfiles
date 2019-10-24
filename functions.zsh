b574662047c0cf41b398d19848a419e487e52817
andhikayuana <andhikayuana@gmail.com>
2019-10-24 16:33:39 +0700

    new function for delete node modules

--- functions.zsh
+++ functions.zsh
@@ -4,3 +4,13 @@ load_config () {
     source $DOTDIR/$1/path.zsh
     source $DOTDIR/$1/aliases.zsh
 }
+
+find_and_delete() {
+#todo filename to param function
+   find ~/.gradle/caches -name "*.lock" -exec rm  "{}" \;
+}
+
+
+delete_node_modules() {
+   find . -name "node_modules" -type d -prune -exec rm -rf '{}' +
+}
